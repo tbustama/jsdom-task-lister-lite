42a1a819a85fc2dd5da011debc619c6f82bece53
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-13 11:04:44 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      const li = document.createElement("li");
+      li.innerText = e.target["new-task-description"].value;
+      document.getElementById("tasks").appendChild(li);
+    });
 });
